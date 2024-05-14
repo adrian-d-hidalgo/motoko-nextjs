3602fe150e6adba91587e58d1ed468705d0cd217
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-05-13 19:35:34 -0600

    chore: example improved

--- backend/test/main.mo
+++ backend/test/main.mo
@@ -1,7 +1,59 @@
 import Principal "mo:base/Principal";
+import HashMap "mo:base/HashMap";
+import Result "mo:base/Result";
+import Bool "mo:base/Bool";
 
 actor {
-    public query (message) func whoAmI () : async Principal {
-        return message.caller;
-    }
+    type Profile = {
+        username : Text;
+        bio : Text;
+    };
+
+    type GetProfileError = {
+        #userNotAuthenticated;
+        #profileNotFound;
+    };
+
+    type GetProfileResponse = Result.Result<Profile, GetProfileError>;
+
+    type CreateProfileError = {
+        #profileAlreadyExists;
+        #userNotAuthenticated;
+    };
+
+    type CreateProfileResponse = Result.Result<Bool, CreateProfileError>;
+
+    let profiles = HashMap.HashMap<Principal, Profile>(0, Principal.equal, Principal.hash);
+
+    public query ({caller}) func getProfile () : async GetProfileResponse {
+        if (Principal.isAnonymous(caller)) return #err(#userNotAuthenticated);
+
+        let profile = profiles.get(caller);
+
+        switch profile {
+            case (?profile) {
+                #ok(profile);
+            };
+            case null {
+                #err(#profileNotFound);
+            };
+        }
+    };
+
+    public shared ({caller}) func createProfile (username : Text, bio : Text) : async CreateProfileResponse {
+        if (Principal.isAnonymous(caller)) return #err(#userNotAuthenticated);
+
+        let profile = profiles.get(caller);
+
+        if (profile != null) return #err(#profileAlreadyExists);
+
+        let newProfile: Profile = {
+            username = username;
+            bio = bio;
+        };
+        
+        profiles.put(caller, newProfile);
+
+        #ok(true);
+    };
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,28 @@
 import { Identity } from "@dfinity/agent";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 import { LogoutButton, useAuth, useCandidActor, useIdentities } from "@bundly/ares-react";
 
 import { CandidActors } from "@app/canisters";
 import Header from "@app/components/header";
 
+type Profile = {
+  username: string;
+  bio: string;
+};
+
 export default function IcConnectPage() {
   const { isAuthenticated, currentIdentity, changeCurrentIdentity } = useAuth();
   const identities = useIdentities();
-  const backend = useCandidActor<CandidActors>("test", currentIdentity) as CandidActors["test"];
+  const [profile, setProfile] = useState<Profile | undefined>();
+  const [loading, setLoading] = useState(false); // State for loader
+  const test = useCandidActor<CandidActors>("test", currentIdentity, {
+    canisterId: process.env.NEXT_PUBLIC_TEST_CANISTER_ID,
+  }) as CandidActors["test"];
+
+  useEffect(() => {
+    getProfile();
+  }, [currentIdentity]);
 
   function formatPrincipal(principal: string): string {
     const parts = principal.split("-");
@@ -21,15 +35,39 @@ export default function IcConnectPage() {
     return currentIdentity.getPrincipal().toString() === identityButton.getPrincipal().toString();
   }
 
-  async function verifyIdentity() {
+  async function getProfile() {
     try {
-      const response = await backend.whoAmI();
-      console.log(
-        "Is the same identity?: ",
-        response.toString() === currentIdentity.getPrincipal().toString()
-      );
+      const response = await test.getProfile();
+
+      if ("err" in response) {
+        if ("userNotAuthenticated" in response.err) console.log("User not authenticated");
+        else console.log("Error fetching profile");
+      }
+
+      const profile = "ok" in response ? response.ok : undefined;
+      setProfile(profile);
     } catch (error) {
       console.error({ error });
+    }
+  }
+
+  async function registerProfile(username: string, bio: string) {
+    try {
+      setLoading(true); // Show loader
+      const response = await test.createProfile(username, bio);
+
+      if ("err" in response) {
+        if ("userNotAuthenticated" in response.err) alert("User not authenticated");
+        if ("profileAlreadyExists" in response.err) alert("Profile already exists");
+
+        throw new Error("Error creating profile");
+      }
+
+      setProfile({ username, bio });
+    } catch (error) {
+      console.error({ error });
+    } finally {
+      setLoading(false); // Hide loader
     }
   }
 
@@ -47,11 +85,19 @@ export default function IcConnectPage() {
               <p className="text-gray-700">
                 <strong>Current Identity:</strong> {currentIdentity.getPrincipal().toString()}
               </p>
-              <button
-                className={"px-3 py-1 text-sm rounded-md bg-blue-500 text-white"}
-                onClick={verifyIdentity}>
-                Verify
-              </button>
+              <h2 className="text-xl font-bold mb-2">Profile</h2>
+              {profile ? (
+                <>
+                  <p>
+                    <strong>Username: </strong> {profile.username}
+                  </p>
+                  <p>
+                    <strong>Bio: </strong> {profile.bio}
+                  </p>
+                </>
+              ) : (
+                <CreateProfileForm onSubmit={registerProfile} loading={loading} />
+              )}
             </div>
 
             <div className="bg-white rounded-lg shadow-md p-6">
@@ -60,7 +106,7 @@ export default function IcConnectPage() {
                 {identities.map((identity, index) => (
                   <li key={index} className="flex items-center justify-between py-4">
                     <span className="text-gray-900">
-                      {formatPrincipal(identity.identity.getPrincipal().toString())}
+                      {identity.provider} : {formatPrincipal(identity.identity.getPrincipal().toString())}
                     </span>
                     <div className="flex gap-2">
                       <button
@@ -83,5 +129,74 @@ export default function IcConnectPage() {
         </div>
       </main>
     </>
+  );
+}
+
+type ProfileFormProps = {
+  onSubmit: (username: string, bio: string) => Promise<void>;
+  loading: boolean; // Loader state
+};
+
+function CreateProfileForm({ onSubmit, loading }: ProfileFormProps) {
+  const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleBioChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setBio(event.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await onSubmit(username, bio);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setUsername("");
+    setBio("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="bio">
+          Bio
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="bio"
+          placeholder="Bio"
+          value={bio}
+          onChange={handleBioChange}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          disabled={loading} // Disable button while loading
+        >
+          {loading ? "Creating Profile..." : "Create Profile"}
+        </button>
+      </div>
+    </form>
   );
 }
